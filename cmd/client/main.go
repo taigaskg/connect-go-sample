6dde9d8fb31b815c11ab83f311326ea4bcda61e3
TaigaSakaguchi <gucci.tennismath@gmail.com>
2022-10-10 03:17:44 +0900

    initial commit

---
+++ cmd/client/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	greetv1 "example/gen/greet/v1"
+	"example/gen/greet/v1/greetv1connect"
+	"log"
+	"net/http"
+
+	"github.com/bufbuild/connect-go"
+)
+
+func main() {
+	client := greetv1connect.NewGreetServiceClient(
+		http.DefaultClient,
+		"http://localhost:8080",
+		connect.WithGRPC(),
+	)
+	res, err := client.Greet(
+		context.Background(),
+		connect.NewRequest(&greetv1.GreetRequest{Name: "taiga"}),
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(res)
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module example
+
+go 1.18
+
+require (
+	github.com/bufbuild/connect-go v1.0.0
+	golang.org/x/net v0.0.0-20221004154528-8021a29435af
+	google.golang.org/protobuf v1.28.1
+)
+
+require golang.org/x/text v0.3.7 // indirect
